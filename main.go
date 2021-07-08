e94bf4a57c0483ea13810531f3914bc1d4d0f171
Daniil Naumetc <daniil.naumetc@gmail.com>
2021-07-08 23:30:49 +0200

    x

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
-type WttrFetcher struct {
+import (
+	"time"
+)
+
+type Forecast struct {
+	CollectedAt time.Time
+	TargetAt    time.Time
+	Type        string // "now", "hourly", "daily"
+	Temperature float32
+	Condition   string
+	Location    string
+	Source      string
 }
 
 type Fetcher interface {
