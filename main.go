e77d21cc6b8f10cbb67466de470ed4ec93df654b
Daniil Naumetc <daniil.naumetc@gmail.com>
2021-07-08 23:53:30 +0200

    x

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
 type Forecast struct {
 	CollectedAt time.Time
 	TargetAt    time.Time
-	Type        string // "now", "hourly", "daily"
+	Type        int32 // "now", "hourly", "daily"
 	Temperature float32
 	Condition   string
 	Location    string
@@ -15,15 +16,41 @@ type Forecast struct {
 }
 
 type Fetcher interface {
-	fetch()
+	fetch(cities []string, forecasts chan Forecast)
 }
 
 type WttrFetcher struct {
 }
 
+func (f *WttrFetcher) fetch(cities []string, forecasts chan Forecast) {
+	for _, city := range cities {
+		if f.hasCity(&city) {
+			f.fetchCity(city, forecasts)
+		}
+	}
+}
+
+func (f *WttrFetcher) hasCity(city *string) bool {
+	return *city == "Berlin"
+}
+
+func (f *WttrFetcher) fetchCity(city string, forecasts chan Forecast) {
+	forecasts <- Forecast{}
+}
+
 func main() {
+	forecasts := make(chan Forecast)
 	cities := []string{
 		"Berlin",
 	}
+	fetcher1 := WttrFetcher{}
 
+	go func() {
+		fetcher1.fetch(cities, forecasts)
+		close(forecasts)
+	}()
+
+	for forecast := range forecasts {
+		fmt.Printf("%v\n", forecast)
+	}
 }
